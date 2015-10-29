959ab0a74f709130409cc5dd6df68c819680fdf6
zjj20151012 <443411162@qq.com>
2015-10-29 22:59:04 +0800

    Initial Commit

---
+++ myMusicPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  myMusicPlayer
+//
+//  Created by HelloWorld on 15/10/29.
+//  Copyright © 2015年 zjj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ myMusicPlayerTests/myMusicPlayerTests.m
@@ -0,0 +1,39 @@
+//
+//  myMusicPlayerTests.m
+//  myMusicPlayerTests
+//
+//  Created by HelloWorld on 15/10/29.
+//  Copyright © 2015年 zjj. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface myMusicPlayerTests : XCTestCase
+
+@end
+
+@implementation myMusicPlayerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
